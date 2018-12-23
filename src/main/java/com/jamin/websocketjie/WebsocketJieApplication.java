9e3ab164301d924cd8de68eb672645b8d50459c6
huangmingjie <huangmingjie@yupiao.com>
2018-12-23 10:41:44 +0800

    整理代码

--- src/main/java/com/jamin/websocketjie/WebsocketJieApplication.java
+++ src/main/java/com/jamin/websocketjie/WebsocketJieApplication.java
@@ -1,9 +1,12 @@
 package com.jamin.websocketjie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class WebsocketJieApplication {
 
     public static void main(String[] args) {
